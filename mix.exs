832431cf9c3134b1c3cd633e5b5d697db853efd6
d0rc <7993863@gmail.com>
2014-06-13 05:58:36 +0400

    fix minor

--- mix.exs
+++ mix.exs
@@ -4,7 +4,6 @@ defmodule Jazz.Mixfile do
   def project do
     [ app: :jazz,
       version: "0.1.0",
-      elixir: "~> 0.14.0",
       package: package,
       description: "JSON handling library for Elixir." ]
   end
